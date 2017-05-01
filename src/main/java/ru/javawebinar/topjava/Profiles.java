bc8572589048e5ab87fd5e472c8742b7d264f8c5
elusive935 <alenka.fml@mail.ru>
2017-05-01 18:59:06 +0300

    6_08_profile_resolver

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -6,16 +6,17 @@ package ru.javawebinar.topjava;
  */
 public class Profiles {
     public static final String
-            POSTGRES_DB = "postgres",
-            HSQL_DB = "hsqldb";
-
-    public static final String
             JDBC = "jdbc",
             JPA = "jpa",
             DATAJPA = "datajpa";
 
     public static final String REPOSITORY_IMPLEMENTATION = JPA;
 
+    private static final String
+            POSTGRES_DB = "postgres",
+            HSQL_DB = "hsqldb";
+
+    //  Get DB profile depending of DB driver in classpath
     public static String getActiveDbProfile() {
         try {
             Class.forName("org.postgresql.Driver");
