d20c0982c410dbd608389859095d95fbf09eb134
elusive935 <alenka.fml@mail.ru>
2017-06-03 09:45:05 +0300

    Start add meal servlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,28 +1,29 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-
 /**
- * User: gkislin
- * Date: 19.08.2014
+ * Created by alena.nikiforova on 26.03.2017.
  */
-public class UserServlet extends HttpServlet {
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(MealServlet.class);
 
-    private static final Logger LOG = getLogger(UserServlet.class);
-    
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LOG.debug("redirect to users");
+        LOG.debug("forward to meals.jsp");
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("users.jsp");
+        request.setAttribute("abc", MealsUtil.getWithExceeded(MealsUtil.meals, MealsUtil.caloriesPerDay));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
+
 }
